4b6c64f6862a983959ab3cad0521501ca2a2a3ba
unoffending <109032978+unoffending@users.noreply.github.com>
2023-05-19 19:30:29 +0600

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first = number/100;
+        int second = (number/10)%10;
+        int third = number%10;
+        System.out.println(third + "" + second + "" + first);
     }
 }
